492528e44e0d56c3e9c59c9f943036a9eaf4c9bc
Joe Maples <joe@frap129.org>
2017-11-01 09:47:37 -0400

    Disable subsytem ramdumps
    Change-Id: I97d92637de4c81f04a9d9cd4737ed844c18e5082

--- device.mk
+++ device.mk
@@ -653,7 +653,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
 # Subsystem ramdump
 PRODUCT_PROPERTY_OVERRIDES += \
-    persist.vendor.sys.ssr.enable_ramdumps=1
+    persist.vendor.sys.ssr.enable_ramdumps=0
 endif
 
 # Subsystem silent restart
