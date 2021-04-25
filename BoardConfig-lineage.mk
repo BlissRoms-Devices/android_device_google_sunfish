63a2918a059504849222c898915d75d47361808a
Jackeagle <jackeagle102@gmail.com>
2021-04-24 22:22:01 -0400

    sunfish: Add init to fix Safetynet CTS Failure
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- BoardConfig-lineage.mk
+++ BoardConfig-lineage.mk
@@ -14,6 +14,8 @@
 # limitations under the License.
 #
 
+DEVICE_PATH := device/google/sunfish
+
 ## Kernel
 BOARD_KERNEL_IMAGE_NAME := Image.lz4
 KERNEL_LD := LD=ld.lld
@@ -42,5 +44,9 @@ BOARD_VENDORIMAGE_PARTITION_RESERVED_SIZE := 30720000
 # Verified Boot
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --set_hashtree_disabled_flag
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 2
+
+# Vendor init
+TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_sunfish
+TARGET_RECOVERY_DEVICE_MODULES := libinit_sunfish
 
 -include vendor/google/sunfish/BoardConfigVendor.mk
