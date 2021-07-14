49906481994ff4d7ade3627860140bbac99531ee
Jackeagle <jackeagle102@gmail.com>
2021-07-14 19:52:18 +0200

    init: Update fingerprint to July Sec release
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- init/init_sunfish.cpp
+++ init/init_sunfish.cpp
@@ -56,5 +56,5 @@ void property_override_dual(char const system_prop[], char const vendor_prop[],
 void vendor_load_properties()
 {
     // fingerprint
-    property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "google/sunfish/sunfish:11/RQ3A.210605.005/7349499:user/release-keys");
+    property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "google/sunfish/sunfish:11/RQ3A.210705.001/7380771:user/release-keys");
 }
