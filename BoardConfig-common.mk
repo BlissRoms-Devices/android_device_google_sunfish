e65bae93268ec00517c6704b29d3e045f4bbef63
PixelBoot <bootingpixels@gmail.com>
2022-01-23 16:18:12 +0100

    sunfish: inherit proper lld definitions

--- BoardConfig-common.mk
+++ BoardConfig-common.mk
@@ -43,13 +43,13 @@ TARGET_BOARD_COMMON_PATH := device/google/sunfish/sm7150
 
 # Kernel
 BOARD_KERNEL_IMAGE_NAME := Image.lz4
-KERNEL_LD := LD=ld.lld
 TARGET_COMPILE_WITH_MSM_KERNEL := true
 TARGET_KERNEL_ADDITIONAL_FLAGS := \
     DTC=$(shell pwd)/prebuilts/tools-custom/$(HOST_OS)-x86/dtc/dtc \
     MKDTIMG=$(shell pwd)/prebuilts/misc/$(HOST_OS)-x86/libufdt/mkdtimg
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_CLANG_COMPILE := true
+KERNEL_SUPPORTS_LLVM_TOOLS := true
 TARGET_KERNEL_CONFIG := sunfish_defconfig
 TARGET_KERNEL_SOURCE := kernel/google/msm-4.14
 TARGET_NEEDS_DTBOIMAGE := true
