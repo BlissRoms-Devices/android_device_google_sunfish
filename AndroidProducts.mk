cf32e8fcdf44da59f39d43996e2752a2b824a7c1
Jackeagle <jackeagle102@gmail.com>
2022-03-05 10:33:37 +0100

    Blissify
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -16,7 +16,9 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/aosp_sunfish.mk \
+    $(LOCAL_DIR)/bliss_sunfish.mk \
     $(LOCAL_DIR)/aosp_sunfish_hwasan.mk \
 
 COMMON_LUNCH_CHOICES := \
     aosp_sunfish-userdebug \
+    bliss_sunfish-userdebug \
--- aosp_sunfish.mk
+++ aosp_sunfish.mk
@@ -42,11 +42,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_product.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/handheld_vendor.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_vendor.mk)
 
-# Inherit some common PixelExperience stuff
-TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_GAPPS_ARCH := arm64
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
-
 $(call inherit-product, device/google/sunfish/device-sunfish.mk)
 $(call inherit-product-if-exists, vendor/google_devices/sunfish/proprietary/device-vendor.mk)
 $(call inherit-product-if-exists, vendor/google_devices/sunfish/prebuilts/device-vendor-sunfish.mk)
