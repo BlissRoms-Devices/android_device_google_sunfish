18af7a54c86d0e6db96910d31326274c01f94df9
Michael Bestas <mkbestas@lineageos.org>
2022-03-28 21:32:12 +0200

    Update fingerprint/build description from SQ1A.220205.002
    Change-Id: I693c5bc8b922a38fce568b118331773f5448a917

--- aosp_sunfish.mk
+++ aosp_sunfish.mk
@@ -74,8 +74,8 @@ PRODUCT_MODEL := Pixel 4a
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_PRODUCT=sunfish \
-    PRIVATE_BUILD_DESC="sunfish-user 12 SQ1A.220205.002 8010174 release-keys"
+    PRIVATE_BUILD_DESC="sunfish-user 12 SP2A.220305.012 8177914 release-keys"
 
-BUILD_FINGERPRINT := google/sunfish/sunfish:12/SQ1A.220205.002/8010174:user/release-keys
+BUILD_FINGERPRINT := google/sunfish/sunfish:12/SP2A.220305.012/8177914:user/release-keys
 
 $(call inherit-product, vendor/google/sunfish/sunfish-vendor.mk)
